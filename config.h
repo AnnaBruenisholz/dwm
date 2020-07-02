10c43f04a3fdcc2aefb765dabd7d67a9961fb011
Alexander Bocken <alexander@bocken.org>
2020-07-02 12:40:33 +0200

    added fibonacci patch

--- config.h
+++ config.h
@@ -38,9 +38,12 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+ 	{ "[@]",      spiral },	   /* first entry is default */
+ 	{ "[\\]",     dwindle },
+	{ "[]=",      tile },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
